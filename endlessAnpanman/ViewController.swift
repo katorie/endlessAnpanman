c7f573368dd11866a5b3e3b3922ece71ebea4c73
katorie <katoriexxxkatorie@gmail.com>
2018-05-02 11:07:27 +0900

    Initial Commit

---
+++ endlessAnpanman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  endlessAnpanman
+//
+//  Created by 唐澤 on 2018/05/02.
+//  Copyright © 2018年 katorie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
