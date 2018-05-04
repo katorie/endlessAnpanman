60d0a742a044d8e5ee16eae09898b79ecdca20eb
katorie <katoriexxxkatorie@gmail.com>
2018-05-05 06:09:07 +0900

    とりあえず動いた！

--- endlessAnpanman/ViewController.swift
+++ endlessAnpanman/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var timer: Timer?
+    var displayImageNo = 0
+    
+    func displayImage() {
+        
+        // TODO xcassetsに要素が増えても大丈夫な書き方に修正
+        let displayImagesArray = [
+            "anpanman01",
+            "anpanman02",
+            "anpanman03",
+            "anpanman04",
+        ]
+        
+        // TODO displayImagesArray の要素数に依存しているので修正
+        if displayImageNo < 0 {
+            displayImageNo = 3
+        }
+        
+        if displayImageNo > 3 {
+            displayImageNo = 0
+        }
+        
+        let name = displayImagesArray[displayImageNo]
+        let image = UIImage(named: name)
+        imageView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO assetesのなかからランダムにとってくることはできる？
+        let image = UIImage(named: "anpanman01")
+        imageView.image = image
+        
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector (ViewController.onTimer(timer:)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func onTimer(timer: Timer) {
+        displayImageNo += 1
+        displayImage()
     }
 
     override func didReceiveMemoryWarning() {
